ff5aa1dcab565701591556b62e55568bc4b2a5ad
David Serrano <david.nonamedguy@gmail.com>
2013-07-31 17:12:33 +0100

    No need for build_win32 now

--- setup.py
+++ setup.py
@@ -28,6 +28,18 @@ build_exe_opt = {
         "include_msvcr" : True
 }
 
+to_delete = [
+        os.path.join("tcl", "encoding"),
+        os.path.join("tcl", "http1.0"),
+        os.path.join("tcl", "msgs"),
+        os.path.join("tcl", "tzdata"),
+        os.path.join("tcl", "opt0.4"),
+        
+        os.path.join("tk", "demos"),
+        os.path.join("tk", "images"),
+        os.path.join("tk", "msgs")
+    ]
+
 
 setup(
         name='clipfiles',
@@ -47,6 +59,14 @@ download all their documents, assignments and exames, with few clicks""",
             'build_exe': build_exe_opt
         }
     )
+
+if len(sys.argv) > 0 and sys.argv[1] == "build":
+    print "Deleting Fat"
+    import shutil
+    for f in to_delete:
+        to_del = os.path.join("build", "exe.%s-2.7" % (sys.platform,),f)
+        print "Removing: %s" % (to_del,)
+        shutil.rmtree(to_del)
 
 if sys.platform == "win32":
     try:
